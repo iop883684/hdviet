ab89e34cfb92402b819d1e70a65a815c5df7ddde
Lee <iop883684@gmail.com>
2019-04-16 21:02:35 +0700

    Updates

---
+++ precache-manifest.c76d134b97f8cf8ca499cfd9ee9e5dd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1fbbe911788476e4527",
+    "url": "/hdviet/static/js/runtime~main.e9037698.js"
+  },
+  {
+    "revision": "0d3db9643b8aee2a68d0",
+    "url": "/hdviet/static/js/main.e6df2594.chunk.js"
+  },
+  {
+    "revision": "4e6cdafa3be35027a72d",
+    "url": "/hdviet/static/js/2.d433e33e.chunk.js"
+  },
+  {
+    "revision": "0d3db9643b8aee2a68d0",
+    "url": "/hdviet/static/css/main.94cf0dae.chunk.css"
+  },
+  {
+    "revision": "4e6cdafa3be35027a72d",
+    "url": "/hdviet/static/css/2.7f90ab0a.chunk.css"
+  },
+  {
+    "revision": "eaa21d312742561068f3610187bde072",
+    "url": "/hdviet/index.html"
+  }
+];
